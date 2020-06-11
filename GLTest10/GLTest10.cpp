d37e0acc03bbb52033733826d59b349ab72f99b9
duzenko <anton.duzenko@gmail.com>
2020-06-11 18:58:03 +0300

    Laptop optimization

--- GLTest10/GLTest10.cpp
+++ GLTest10/GLTest10.cpp
@@ -50,3 +50,8 @@ int main() {
 
     glfwTerminate();
 }
+
+extern "C" {
+    __declspec( dllexport ) int NvOptimusEnablement = 0x00000001;
+    __declspec( dllexport ) int AmdPowerXpressRequestHighPerformance = 1;
+}
