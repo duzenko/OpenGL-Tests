f641078ff3b5bb63dfef9637a6cfce266234da38
duzenko <anton.duzenko@gmail.com>
2020-06-18 20:44:39 +0300

    Access violation fix

--- GLTest11/Image.cpp
+++ GLTest11/Image.cpp
@@ -12,6 +12,7 @@ void Image::Bind() {
         glBindTexture( GL_TEXTURE_2D, texHandle );
         glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
         glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
+        glPixelStorei( GL_UNPACK_ALIGNMENT, 1 );
         glTexImage2D( GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data );
     }
     printf( "Bind texHandle %d\n", texHandle );
